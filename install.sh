118346acd321c290ddece2fcebbebcf729cf53ce
NaOHCC <46294137+NaOHCC@users.noreply.github.com>
2023-07-06 23:30:46 +0800

    fix: install.sh 正确检查退出代码

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# 检查~/.zshrc文件是否存在"source ~/shprofile/bash_aliases"这行
-grep -q "source ~/shprofile/bash_aliases" ~/.zshrc
 
-# 检查上一条命令的退出状态
-if [[ $? -ne 0 ]]; then
+# 检查~/.zshrc文件是否存在"source ~/shprofile/bash_aliases"这行
+if ! grep -q "source ~/shprofile/bash_aliases" ~/.zshrc; then
     # 如果没有找到目标行，则将它追加到文件末尾
     echo "source ~/shprofile/bash_aliases" >> ~/.zshrc
     echo "已添加到~/.zshrc文件中"
