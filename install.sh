768e1102f284b01015551492eec278d9c7ced726
NaOHCC <46294137+NaOHCC@users.noreply.github.com>
2024-06-21 18:51:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@ script_dir=$(dirname "$0")
 echo "The script is located in: $script_dir"
 
 install_chezmoi() {
-    apt install git
+    apt install git -y
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --source-path "$script_dir"
 }
 
 install_zsh() {
-    apt install zsh
+    apt install zsh -y
     chsh -s "$(which zsh)"
 }
 
+apt update
 install_zsh
 install_chezmoi
